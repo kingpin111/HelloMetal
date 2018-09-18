68d97e9c7c018bdeaea2ff63796490a4a1b5cd69
Victor <kingpin11@naver.com>
2018-09-18 18:22:53 +0900

    MTLDevice 선언 및 초기화 추가

--- HelloMetal/HelloMetal/ViewController.swift
+++ HelloMetal/HelloMetal/ViewController.swift
@@ -6,20 +6,15 @@
 //  Copyright © 2018년 Victor K Lee. All rights reserved.
 //
 
+import Metal
 import UIKit
 
 class ViewController: UIViewController {
-
+    var device: MTLDevice! //초기화 구문이 아닌 viewDidLoad에서 초기화할 것이므로 Optional 선언, 하지만 명백히 초기화 될 것이므로 implicitly unrapped optional로 선언하여 사용 편의성 제공
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        device = MTLCreateSystemDefaultDevice()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
